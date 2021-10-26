bf7336e99463d30c7c0dc7795398940c7dadb0e1
Jessica-13 <jessica.spera@insa-lyon.fr>
2021-10-26 16:32:03 +0200

    modif main output

--- src/main.go
+++ src/main.go
@@ -11,7 +11,16 @@ type line struct {
   }
 
 func (l *line) describe() {
-	fmt.Printf(" %v - %v - %v \n", l.id, l.predecessor, l.distance)
+	fmt.Printf("  %v -      %v      -    %v \n", l.id, l.predecessor, l.distance)
+	/*for id, predecessor,distance := range ex_val_distance {
+        fmt.Printf("%c - %c = %d\n", id, predecessor, distance)
+    }*/
+	/*
+	for i := range ex_val_distance {
+		fmt.Printf("%c - ", id)
+		fmt.Printf("%c = ", predecessor)
+        fmt.Printf("%d\n", distance)
+    }*/
   }
 
 func (l *line) setId(id string) {
@@ -28,15 +37,29 @@ func (l *line) setDistance(distance int) {
 
 func main() {
 	ll := &line{id: "A", predecessor: "R", distance: 1}
-	ll.describe()
+	/*
 	//declaration empty slice
-	var ex_val_distance []int										//ex_val_distance = make([]int,3,8)
+	var ex_val_distance line										//ex_val_distance = make([]int,3,8)   ---   ex_val_distance[0]=10
 	// join element 10
 	ex_val_distance=append(ex_val_distance,10)
 	// join element 10
 	ex_val_distance=append(ex_val_distance,20)
 	// print content and dimensions
 	fmt.Println(ex_val_distance)
-	fmt.Println(len(ex_val_distance))	//length
-	fmt.Println(cap(ex_val_distance))	//capacity
+	//fmt.Println(len(ex_val_distance))	//length
+	//fmt.Println(cap(ex_val_distance))	//capacity
+	
+	var ex_val_distance[10] string
+	for x := 0; x < 3; x++ {
+		ex_val_distance[x] = "A"
+	}
+	for i := range ex_val_distance {
+		ll.setId("B")
+		ll.setPredecessor("C")
+		ll.setDistance(3)
+		ll.describe()
+    }
+	*/
+	fmt.Println(" id - predecessor - distance ")
+	ll.describe()
   }
