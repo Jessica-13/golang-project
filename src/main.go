c7cf14a08217f52da82d77bb1796aa9d81984f54
Jessica-13 <jessica.spera@insa-lyon.fr>
2021-11-07 15:29:57 +0100

    add modif

--- src/main.go
+++ src/main.go
@@ -1,20 +1,120 @@
 package main
 
+import hp "container/heap" 
+
 import (
 	"fmt"
 )
 
-/*
-		Graph initialization
-*/
+// heap
+type path struct {
+    value int
+    nodes []string
+}
 
-/*
-		Dijkstra integration <- calculDisMin.go
+type minPath []path
 
-		-> Need return: 	ex_val_id := [n]string{}
-							ex_val_predecessor := [n]string{}
-							ex_val_distance := [n]int{}
-*/
+func (h minPath) Len() int { 
+	return len(h) 
+}
+
+func (h minPath) Less(i, j int) bool { 
+	fmt.Println("h[i]", h[i])
+	fmt.Println("h[j]", h[j])
+	fmt.Println("h[i].value < h[j].value", h[i].value < h[j].value)
+	return h[i].value < h[j].value 
+}
+
+func (h minPath) Swap(i, j int)      { 
+	h[i], h[j] = h[j], h[i] 
+}
+
+func (h *minPath) Push(x interface{}) {
+    *h = append(*h, x.(path))
+}
+
+func (h *minPath) Pop() interface{} {
+    old := *h
+    n := len(old)
+    x := old[n-1]
+    *h = old[0 : n-1]
+    return x
+}
+
+type heap struct {
+    values *minPath
+}
+
+func newHeap() *heap {
+    return &heap{values: &minPath{}}
+}
+
+func (h *heap) push(p path) {
+    hp.Push(h.values, p)
+}
+
+func (h *heap) pop() path {
+    i := hp.Pop(h.values)
+    return i.(path)
+}
+
+// END heap
+
+// graphe 
+type edge struct {
+    node   string
+    weight int
+}
+
+type graph struct {
+    nodes map[string][]edge
+}
+
+func newGraph() *graph {
+    return &graph{nodes: make(map[string][]edge)}
+}
+
+func (g *graph) addEdge(origin, destiny string, weight int) {
+    g.nodes[origin] = append(g.nodes[origin], edge{node: destiny, weight: weight})
+    g.nodes[destiny] = append(g.nodes[destiny], edge{node: origin, weight: weight})
+}
+
+func (g *graph) getEdges(node string) []edge {
+    return g.nodes[node]
+}
+
+func (g *graph) getPath(origin, destiny string) (int, []string) { //gets the shortest path between origin and destiny
+    h := newHeap()
+    h.push(path{value: 0, nodes: []string{origin}})
+    visited := make(map[string]bool)
+
+    for len(*h.values) > 0 {
+        // Find the nearest yet to visit node
+        p := h.pop()
+        node := p.nodes[len(p.nodes)-1]
+
+        if visited[node] {
+            continue
+        }
+
+        if node == destiny {
+            return p.value, p.nodes
+        }
+
+        for _, e := range g.getEdges(node) {
+            if !visited[e.node] {
+                // We calculate the total spent so far plus the cost and the path of getting here
+                h.push(path{value: p.value + e.weight, nodes: append([]string{}, append(p.nodes, e.node)...)})
+            }
+        }
+
+        visited[node] = true
+    }
+
+    return 0, nil
+}
+
+// END graph
 
 type line struct {
 	id string
@@ -52,4 +152,20 @@ func main() {
 		ll := &line{id: ex_val_id[i], predecessor: ex_val_predecessor[i], distance: ex_val_distance[i]}
 		ll.describe()
 	}	
+
+	fmt.Println("Dijkstra")
+    // Example
+    graph := newGraph()
+    graph.addEdge("S", "B", 4)
+    graph.addEdge("S", "C", 2)
+    graph.addEdge("B", "C", 1)
+    graph.addEdge("B", "D", 5)
+    graph.addEdge("C", "D", 8)
+    graph.addEdge("C", "E", 10)
+    graph.addEdge("D", "E", 2)
+    graph.addEdge("D", "T", 6)
+    graph.addEdge("E", "T", 2)
+    fmt.Println(graph.getPath("S", "T"))
+
+
   }
