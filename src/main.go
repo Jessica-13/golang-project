b27513a4599e4344a8074411627816fa48fffe04
Jessica-13 <jessica.spera@insa-lyon.fr>
2021-11-12 16:58:14 +0100

    start of writing comments

--- src/main.go
+++ src/main.go
@@ -7,30 +7,75 @@ import (
 	//"math/rand"
 )
 
-// heap
+// START heap definition
+
+/* Definition of "path" structure :
+non-default type consisting of an integer (named value) and a list of strings (named nodes)
+*/
 type path struct {
     value int
     nodes []string
 }
 
+/* Definition of "minPath" type :
+non-default type consisting of an anonymous list of path 
+*/
 type minPath []path
 
+/* Function closures
+Go language provides a special feature known as an anonymous function. 
+An anonymous function can form a closure. 
+A closure is a special type of anonymous function that references variables 
+declared outside of the function itself. 
+It is similar to accessing global variables which are available 
+before the declaration of the function.
+
+Explanation: 
+The closure references the variable GFG even after the newCounter() function 
+has finished running but no other code outside of the newCounter() function 
+has access to this variable.
+*/
+
+/* Definition anonymous fonction that : 
+input  -> h (minPath type) 
+-> call to the Len() function
+output -> length of h (int type) 
+*/
 func (h minPath) Len() int { 
 	return len(h) 
 }
 
+/* Definition anonymous fonction that : 
+input  -> h (minPath type)
+-> call to the Less() function 
+output -> the shorter distance between the two provided
+  return True -> h[i].value is the shorter one 
+  return False -> h[j].value is the shorter one
+
+h[i] -> case at index i of list h
+.valure -> access to attribute value of the case h[]
+*/
 func (h minPath) Less(i, j int) bool { 
 	return h[i].value < h[j].value 
 }
 
+/* Definition anonymous fonction that : 
+input  -> h (minPath type)
+output -> If the second h[j] is the shorter one, 
+          inversion of the two to be able to compare it with the next value
+*/
 func (h minPath) Swap(i, j int)      { 
 	h[i], h[j] = h[j], h[i] 
 }
 
+/*
+*/
 func (h *minPath) Push(x interface{}) {
     *h = append(*h, x.(path))
 }
 
+/*
+*/
 func (h *minPath) Pop() interface{} {
     old := *h
     n := len(old)
@@ -39,49 +84,71 @@ func (h *minPath) Pop() interface{} {
     return x
 }
 
+/* Definition of "heap" structure :
+non-default type consisting of a pointer of minpath (named values)
+*/
 type heap struct {
     values *minPath
 }
 
+/*
+*/
 func newHeap() *heap {
     return &heap{values: &minPath{}}
 }
 
+/*
+*/
 func (h *heap) push(p path) {
     hp.Push(h.values, p)
 }
 
+/*
+*/
 func (h *heap) pop() path {
     i := hp.Pop(h.values)
     return i.(path)
 }
 
-// END heap
+// END heap definition
 
-// graphe 
+// START graphe definition
 
+/* Definition of "edge" structure :
+non-default type consisting of a string (named node) and an integer (named weight)
+*/
 type edge struct {
     node   string
     weight int
 }
 
+/*
+*/
 type graph struct {
     nodes map[string][]edge
 }
 
+/*
+*/
 func newGraph() *graph {
     return &graph{nodes: make(map[string][]edge)}
 }
 
+/*
+*/
 func (g *graph) addEdge(origin, destiny string, weight int) {
     g.nodes[origin] = append(g.nodes[origin], edge{node: destiny, weight: weight})
     g.nodes[destiny] = append(g.nodes[destiny], edge{node: origin, weight: weight})
 }
 
+/*
+*/
 func (g *graph) getEdges(node string) []edge {
     return g.nodes[node]
 }
 
+/*
+*/
 func (g *graph) getPath(origin, destiny string) (int, []string) { //gets the shortest path between origin and destiny
     h := newHeap()
     h.push(path{value: 0, nodes: []string{origin}})
@@ -113,10 +180,11 @@ func (g *graph) getPath(origin, destiny string) (int, []string) { //gets the sho
     return 0, nil
 }
 
-// END graph
+// END graph definition
 
 
-/* SECTION OUTPUT
+/* START OUTPUT definition
+
 type line struct {
 	id string
 	predecessor string
@@ -159,9 +227,8 @@ func main() {
 
 	fmt.Println("Dijkstra")
     // Example
-    graph := newGraph()
+    graph := newGraph() 
 
-	
     graph.addEdge("S", "B", 4)
     graph.addEdge("S", "C", 2)
     graph.addEdge("B", "C", 1)
